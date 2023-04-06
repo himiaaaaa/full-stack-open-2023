ca553183e08f0bd951590c3b489f19dddb341f60
Miao <himia301@gmail.com>
2023-04-06 16:47:32 +0800

    5.18 exercise

--- part5/bloglist-frontend/cypress/e2e/bloglist_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/bloglist_app.cy.js
@@ -1,6 +1,12 @@
 describe('Bloglist app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'one',
+      username: 'testuserone',
+      password: 'numberone'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -10,4 +16,18 @@ describe('Bloglist app', function() {
     cy.contains('password')
   })
 
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('testuserone')
+      cy.get('#password').type('numberone')
+      cy.get('#login-button').click()
+      cy.contains('one logged in')
+    })
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('testuserone')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+      cy.get('.error').contains('Wrong username or password')
+    })
+  })
 })
