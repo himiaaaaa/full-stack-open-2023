0300c2f04da8e1b3e56a306336de20e7ab5601bd
Miao <himia301@gmail.com>
2023-04-07 12:21:28 +0800

    6.1 - 6.2 exercises

---
+++ part6/unicafe-redux/src/index.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import { createStore } from 'redux'
+import reducer from './reducer'
+
+const store = createStore(reducer)
+
+const App = () => {
+  const good = () => {
+    store.dispatch({
+      type: 'GOOD'
+    })
+  }
+
+  const ok = () => {
+    store.dispatch({
+      type: 'OK'
+    })
+  }
+
+  const bad = () => {
+    store.dispatch({
+      type: 'BAD'
+    })
+  }
+
+  const zero = () => {
+    store.dispatch({
+      type: 'ZERO'
+    })
+  }
+  return (
+      <div>
+        <button onClick={good}>good</button> 
+        <button onClick={ok}>ok</button> 
+        <button onClick={bad}>bad</button>
+        <button onClick={zero}>reset stats</button>
+        <div>good {store.getState().good}</div>
+        <div>ok {store.getState().ok}</div>
+        <div>bad {store.getState().bad}</div>
+      </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+  root.render(<App />)
+}
+
+renderApp()
+store.subscribe(renderApp)
