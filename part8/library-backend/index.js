4f507919b172ca051968cf2d6b42c9b781e8206f
Miao <himia301@gmail.com>
2023-06-06 13:25:28 +0800

    8.3 exercise

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -27,12 +27,6 @@ let authors = [
   },
 ]
 
-/*
- * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
-*/
-
 let books = [
   {
     title: 'Clean Code',
@@ -85,15 +79,12 @@ let books = [
   },
 ]
 
-/*
-  you can remove the placeholder query once your first own has been implemented 
-*/
-
 const typeDefs = `
   type Author {
     name: String!
     id: ID!
     born: Int
+    bookCount: Int
   }
   type Book {
     title: String!
@@ -106,6 +97,7 @@ const typeDefs = `
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -114,6 +106,10 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => authors,
+  },
+  Author: {
+    bookCount: (root) => books.filter(b => b.author === root.name).length
   }
 }
 
