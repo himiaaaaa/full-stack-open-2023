5182ac3b5a84c404b84e40e65f358696f4c57501
Miao <himia301@gmail.com>
2023-06-06 13:39:11 +0800

    8.4 exercise

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -96,7 +96,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String!): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -105,7 +105,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, arg) => books.filter(b => b.author === arg.author),
     allAuthors: () => authors,
   },
   Author: {
