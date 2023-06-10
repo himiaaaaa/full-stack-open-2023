6e594e49f684876897354a07e1a06f84a3223f86
Miao <himia301@gmail.com>
2023-06-10 18:33:21 +0800

    remove useless comment

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -23,103 +23,7 @@ mongoose.connect(MONGODB_URI)
   .catch((error) => {
     console.log('error connection to MongoDB:', error.message)
   })
-
- /*  const author = new Author({ name: 'Martin Fowler', born: 1963 });
-  author.save();
-
- const books = new Book({
-  title: 'Refactoring, edition 2',
-  published: 2018,
-  author: author._id,
-  genres: ['refactoring']
-  })
-
-  books.save().then(result => {
-    console.log('book saved!')
-    mongoose.connection.close()
-  }) 
- */
-
-
-/*   let authors = [
-    {
-      name: 'Robert Martin',
-      id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-      born: 1952,
-    },
-    {
-      name: 'Martin Fowler',
-      id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-      born: 1963
-    },
-    {
-      name: 'Fyodor Dostoevsky',
-      id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-      born: 1821
-    },
-    { 
-      name: 'Joshua Kerievsky', // birthyear not known
-      id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-    },
-    { 
-      name: 'Sandi Metz', // birthyear not known
-      id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-    },
-  ]
   
-  let books = [
-    {
-      title: 'Clean Code',
-      published: 2008,
-      author: 'Robert Martin',
-      id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-      genres: ['refactoring']
-    },
-    {
-      title: 'Agile software development',
-      published: 2002,
-      author: 'Robert Martin',
-      id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-      genres: ['agile', 'patterns', 'design']
-    },
-    {
-      title: 'Refactoring, edition 2',
-      published: 2018,
-      author: 'Martin Fowler',
-      id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-      genres: ['refactoring']
-    },
-    {
-      title: 'Refactoring to patterns',
-      published: 2008,
-      author: 'Joshua Kerievsky',
-      id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-      genres: ['refactoring', 'patterns']
-    },  
-    {
-      title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-      published: 2012,
-      author: 'Sandi Metz',
-      id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-      genres: ['refactoring', 'design']
-    },
-    {
-      title: 'Crime and punishment',
-      published: 1866,
-      author: 'Fyodor Dostoevsky',
-      id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-      genres: ['classic', 'crime']
-    },
-    {
-      title: 'The Demon ',
-      published: 1872,
-      author: 'Fyodor Dostoevsky',
-      id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-      genres: ['classic', 'revolution']
-    },
-  ] */
-  
-
 const typeDefs = `
   type User {
     username: String!
@@ -162,7 +66,7 @@ const typeDefs = `
     ): Author
     createUser(
       username: String!
-     
+      favoriteGenre: String!
     ): User
     login(
       username: String!
@@ -170,7 +74,7 @@ const typeDefs = `
     ): Token
   }
 `
-//favoriteGenre: String!
+
 
 const resolvers = {
   Query: {
