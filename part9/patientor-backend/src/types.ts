680318ef86582687c8560b834d524a1613ee7d8a
Miao <himia301@gmail.com>
2023-06-24 20:49:12 +0800

    9.22 exercise

--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -10,9 +10,9 @@ export interface Diagnose {
     latin?: string;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
+/* // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface Entry {
-}
+} */
 
 export interface Patient {
     id: string;
@@ -29,3 +29,48 @@ export type NoSsnPatient = Omit<Patient, 'ssn'>;
 export type NewPatient = Omit<Patient, 'id'>;
 
 export type NonSensitivePatient = Omit<Patient, 'ssn' | 'entries'>;
+export interface BaseEntry {
+    id: string;
+    description: string;
+    date: string;
+    specialist: string;
+    diagnosisCodes?: Array<Diagnose['code']>;
+}
+
+export enum HealthCheckRating {
+    "Healthy" = 0,
+    "LowRisk" = 1,
+    "HighRisk" = 2,
+    "CriticalRisk" = 3
+}
+  
+export interface HealthCheckEntry extends BaseEntry {
+    type: "HealthCheck";
+    healthCheckRating: HealthCheckRating;
+}
+
+export interface SickLeave {
+    startDate: string;
+    endDate: string;
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+    type: "OccupationalHealthcare";
+    employerName: string;
+    sickLeave?: SickLeave;
+}
+
+export interface Discharge {
+    date: string;
+    criteria: string;
+}
+
+export interface HospitalEntry extends BaseEntry {
+    type: "Hospital";
+    discharge: Discharge;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry; 
